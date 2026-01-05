359e64850059651c2dbefcccd2cf4471d0e96b4a
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2026-01-05 10:48:53 +0600

    server running

--- server/main.js
+++ server/main.js
@@ -2,12 +2,10 @@
 
 const express = require('express')
 const app = express()
-const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+
+
+app.listen(8000, () => {
+  console.log('server is runnig on port 8000')
 })
